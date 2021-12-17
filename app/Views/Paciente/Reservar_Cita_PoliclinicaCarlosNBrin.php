6714a1f0a884c4f4f65945cfe4809f0cbc9ebe9c
root <root@mail.info7.com>
2021-12-16 23:56:41 -0500

    modificacion policlinica don alejandro de la guardia

--- app/Views/Paciente/Reservar_Cita_PoliclinicaCarlosNBrin.php
+++ app/Views/Paciente/Reservar_Cita_PoliclinicaCarlosNBrin.php
@@ -72,7 +72,7 @@ if (isset($_POST['ingresar_cita'])){
         </section>
         <section class="menu_sistema">
             <div class="div_menu_sistema">
-                <h3><a class="btn_reservarcitahover" href="#"><font color="#3498DB">Reservar Citas</font></a></h3>
+                <h3><a class="btn_reservarcitahover" href="Escoger_Centro_Hospitalario.php"><font color="#3498DB">Reservar Citas</font></a></h3>
                 <h3><a class="btn_reservarcitahover" href="citasrecientes.php">Citas Recientes</a></h3>
                 <h3><a class="btn_reservarcitahover" href="<?php $root;?>/Views/Home/pfcontacto.php">Contáctenos</a></h3>
             </div>
